835eaee329da23717b6af1d7cd5f0b3edd48e712
Bud <bla10@pct.edu>
2026-03-01 23:52:35 -0500

    Set up connection to Google Firestore and created basic CRUD functions; helper functions for querying database to come later. Made README actually useable and not empty.

--- README.md
+++ README.md
@@ -1,1 +1,26 @@
 # cit460
+
+This project is a Python program that uses Computer Vision technology to provide business support functions to a retail establishment.
+
+Its core features will ultimately include:
+ -the ability for cameras to observe the shelving units throughout the store and identify when an item is low on stock, prompting an alert that management can review at their convenience without having to send an employee to visually identify where a restock is required. 
+
+-the ability to identify when an incorrect item has been placed on a shelf among other objects, again generating an alert for management to become aware of the need to reorganize. 
+
+-tracking foot traffic throughout the store, providing data (likely via heatmap) about what departments are popular over time periods such as day, week, or month
+
+The basic Computer Vision functionality is provided by OpenCV, an open-source computer vision library that has been in operation since 2000.
+https://opencv.org/about/
+`pip install opencv-python`
+
+Rather than set up machine learning training on personal objects, at time of writing this project is implementing Ultralytics YOLO, another open-source library that includes some pre-trained datasets to get the project up and running quickly with the knowledge of what objects like people, chairs, and drink receptacles.
+https://www.ultralytics.com/about
+https://docs.ultralytics.com/
+`pip install ultralytics`
+
+At time of writing, the backend responsible for handling this information is Google Firestore, a Cloud-based, Document NoSQL platform offered by Google through the Firebase ecosystem.
+https://firebase.google.com/docs/reference
+`pip install google-cloud-firestore`
+
+With experience implementing Firestore in a React application, a brief YouTube tutorial by user "Code First with Hala" was extremely helpful in establishing the syntax differences with a Python app.
+https://www.youtube.com/playlist?list=PLs3IFJPw3G9LW-rGJ8EBMaCd8OxGm_qQe
--- firebase_use.py
+++ firebase_use.py
@@ -1,3 +1,57 @@
-import firebase
+#imports and initialize
 import firebase_admin
+from firebase_admin import firestore
+from firebase_admin import credentials
 
+cred = credentials.Certificate("adv-program-firebase-adminsdk-fbsvc-88f8cc8cbc.json")
+firebase_admin.initialize_app(cred)
+
+db = firestore.client()
+test_collection = "test"
+test_document = "test_2"
+test_data = {'timestamp': firestore.firestore.SERVER_TIMESTAMP, 'Quantity_On_Hand': 10}
+test_data_2 = {'timestamp': firestore.firestore.SERVER_TIMESTAMP, 'Quantity_On_Hand': 2}
+
+#CRUD operations
+#Create
+def create_doc(collection, data):
+    db.collection(collection).add(data)
+
+def create_doc_with_id(collection, doc_id, data): #if you know the specific document id you want to update
+    db.collection(collection).document(doc_id).set(data)
+
+def create_doc_auto_id(collection, data): 
+    db.collection(collection).document().set(data)
+
+def create_doc_merge(collection, doc_id, data):
+    db.collection(collection).document(doc_id).set(data, merge=True)
+
+#Read
+def read_doc_with_id(collection, doc_id): #Only usable if document ID known, unlikely in multi-user app
+    result = db.collection(collection).document(doc_id).get()
+    if result.exists:
+        print(result.to_dict())
+
+def read_all_docs(collection):
+    docs = db.collection(collection).get()
+    for doc in docs:
+        print(doc.to_dict())
+
+#Query
+def read_docs_query(collection):
+    docs = db.collection(collection).where("Quantity_On_Hand", "<", 4).get()
+    for doc in docs:
+        print(doc.to_dict())
+
+#Update
+def update_doc_with_id(collection, doc_id, field, new_value):
+    db.collection(collection).document(doc_id).update({field: new_value})
+
+#Delete
+def delete_doc_with_id(collection, doc_id):
+    db.collection(collection).document(doc_id).delete()
+
+def delete_field_with_id(collection, doc_id, field):
+    db.collection(collection).document(doc_id).update({field:firestore.firestore.DELETE_FIELD})
+
+update_doc_with_id("test", "test_2", "Quantity_On_Hand", 1)
