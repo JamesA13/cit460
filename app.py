11c068030809e154c323ffc3ffb13576565893d7
Bud <bla10@pct.edu>
2026-04-27 02:58:14 -0400

    Implemented CRUD operations into main app flow to generate database entries while camera running. Need to implement automatic checking functionality instead of manual interaction.

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 import cv2
 from ultralytics import YOLO
 import random
+from firebase_use import *
 
 model = YOLO('yolo26n.pt')
 
@@ -13,7 +14,10 @@ frame_height = int(cam.get(cv2.CAP_PROP_FRAME_HEIGHT))
 
 # Define the codec and create VideoWriter object
 print("Please enter a filename for the next recording:")
-filename = input()
+# filename = input()
+filename = 'test'
+print("Please enter the desired object code:")
+currentCls = input()
 fourcc = cv2.VideoWriter_fourcc(*'mp4v')
 out = cv2.VideoWriter(filename + '.mp4', fourcc, 20.0, (frame_width, frame_height))
 
@@ -37,6 +41,52 @@ def showDetections(img, detections, threshold): #frame, results[list] from model
 
             cv2.putText(img, boxLabel, (x1 + 10, y1 - baseline + 5), cv2.FONT_HERSHEY_SIMPLEX, 1, (0,0,0), 1)
 
+targetMin = 2
+
+#Provide the list output of the model detection method and tally a count of the specified object detected in a given frame
+#Method accepts targetCls argument, an integer value corresponding to one of the possible outputs from the detection model
+#For brevity, possible outputs are stored in outputs.txt
+def trackQuantity(detections, targetCls):
+    # targetCls = 39 Class value of 'bottle' detection by Ultralytics
+    currentCount = 0
+
+    boxes = detections.boxes
+    for box in boxes:
+        if int(box.cls[0]) == targetCls:
+            currentCount += 1
+
+    return currentCount
+
+def checkLowQty(detections, minQty, targetCls):
+    print("Checking qty")
+    qoh = trackQuantity(detections, targetCls)
+    if qoh <= minQty:
+        #generate log and alert
+        try:
+            update_doc_with_id("qoh_logs", str(targetCls), "qoh", qoh)
+            update_doc_with_id("qoh_logs", str(targetCls), "timestamp", firestore.firestore.SERVER_TIMESTAMP)
+        except:
+            create_doc_with_id("qoh_logs", str(targetCls), {"qoh": qoh, "timestamp": firestore.firestore.SERVER_TIMESTAMP})
+    print("Checking qty complete")
+    return
+    
+def checkIncorrectItem(detections, threshold, targetCls):
+    boxes = detections.boxes
+    for box in boxes:
+        if int(box.cls[0]) != targetCls:
+            try:
+                update_doc_with_id("placement_error_logs", str(targetCls), "timestamp", firestore.firestore.SERVER_TIMESTAMP)
+            except:
+                create_doc_with_id("placement_error_logs", str(targetCls), {"timestamp": firestore.firestore.SERVER_TIMESTAMP})
+    return
+
+def checkTimestamp(collection, doc_id):
+    timestamp = read_doc_with_id(collection, doc_id)
+    print(timestamp)
+    print(type(timestamp))
+    return
+
+
 print("Beginning recording for " + filename)
 while True:
     ret, frame = cam.read()
@@ -49,9 +99,12 @@ while True:
 
     # Display the captured frame
     cv2.imshow('Camera', frame)
-    
-    # for r in results:
-    #     print(r.boxes)
+
+    # Press 'l' to run low quantity check, 'i' to run incorrect item check
+    if cv2.waitKey(1) == ord('l'):
+        checkLowQty(results, 2, 39)
+    elif cv2.waitKey(1) == ord('i'):
+        checkIncorrectItem(results, .6, currentCls)
 
     # Press 'q' to exit the loop
     if cv2.waitKey(1) == ord('q'):
--- firebase_use.py
+++ firebase_use.py
@@ -2,21 +2,19 @@
 import firebase_admin
 from firebase_admin import firestore
 from firebase_admin import credentials
+import datetime
 
 cred = credentials.Certificate("adv-program-firebase-adminsdk-fbsvc-88f8cc8cbc.json")
 firebase_admin.initialize_app(cred)
 
 db = firestore.client()
-test_collection = "test"
-test_document = "test_2"
-test_data = {'timestamp': firestore.firestore.SERVER_TIMESTAMP, 'Quantity_On_Hand': 10}
-test_data_2 = {'timestamp': firestore.firestore.SERVER_TIMESTAMP, 'Quantity_On_Hand': 2}
 
 #CRUD operations
 #Create
 def create_doc(collection, data):
     db.collection(collection).add(data)
 
+#if used on an existing document will update document fields    ex. create_doc_with_id("test", "new_doc", {"test_field_create": "test_value_create_2"})
 def create_doc_with_id(collection, doc_id, data): #if you know the specific document id you want to update
     db.collection(collection).document(doc_id).set(data)
 
@@ -30,7 +28,7 @@ def create_doc_merge(collection, doc_id, data):
 def read_doc_with_id(collection, doc_id): #Only usable if document ID known, unlikely in multi-user app
     result = db.collection(collection).document(doc_id).get()
     if result.exists:
-        print(result.to_dict())
+        return result.to_dict()
 
 def read_all_docs(collection):
     docs = db.collection(collection).get()
@@ -44,6 +42,7 @@ def read_docs_query(collection):
         print(doc.to_dict())
 
 #Update
+#Does NOT dynamically create a non-existent document    ex. update_doc_with_id("test", "nonexist_doc", "test_field_upd", "test_value_upd")
 def update_doc_with_id(collection, doc_id, field, new_value):
     db.collection(collection).document(doc_id).update({field: new_value})
 
@@ -53,5 +52,3 @@ def delete_doc_with_id(collection, doc_id):
 
 def delete_field_with_id(collection, doc_id, field):
     db.collection(collection).document(doc_id).update({field:firestore.firestore.DELETE_FIELD})
-
-update_doc_with_id("test", "test_2", "Quantity_On_Hand", 1)
